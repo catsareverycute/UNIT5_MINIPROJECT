ec70483bce3e44a145abb1ab3458a5e9d2b01b28
catsareverycute <corinaq3@nycstudents.net>
2024-12-19 22:17:29 -0500

    finished javadocs and wrote test cases w/comments

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,68 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Time time1 = new Time(8, 9, 58);
-        System.out.println(time1);
+        Time time1 = new Time(10,10,58); // New time object (10:10:58)
+        System.out.println("Expected Result: 10:10:58");
+        System.out.println("Actual Result: " + time1);
+        System.out.println();
+
         time1.tick();
-        System.out.println(time1);
+        System.out.println("Expected Result: 10:10:59");
+        System.out.println("Actual Result: " + time1);
+        System.out.println();
+
         time1.tick();
-        System.out.println(time1);
-        time1.tick();
-        System.out.println(time1);
-        time1.tick();
-        System.out.println(time1);
-        time1.tick();
-        time1.tick();
-        time1.tick();
-        time1.tick();
-        time1.tick();
-        time1.tick();
-        time1.tick();
-        time1.tick();
-        System.out.println(time1);
-        Time time2 = new Time(15, 59, 58);
-        System.out.println(time2);
+        System.out.println("Expected Result: 10:11:00"); // Seconds resets to 00 (single digits padded with zero)
+        System.out.println("Actual Result: " + time1);
+        System.out.println();
+
+        Time time2 = new Time(6,59,59); // New time object (06:59:59)
+        System.out.println("Expected Result: 06:59:59");
+        System.out.println("Actual Result: " + time2);
+        System.out.println();
+
         time2.tick();
-        System.out.println(time2);
-        time2.tick();
-        System.out.println(time2);
-        time2.tick();
-        System.out.println(time2);
-        Time time3 = new Time(23, 59, 58);
-        System.out.println(time3);
+        System.out.println("Expected Result: 07:00:00"); // Both minutes and seconds reset to 00
+        System.out.println("Actual Result: " + time2);
+        System.out.println();
+    
+        Time time3 = new Time(23, 59, 59); // New time object (23:59:59)
+        System.out.println("Expected Result: 23:59:59");
+        System.out.println("Actual Result: " + time3);
+        System.out.println();
+
         time3.tick();
-        System.out.println(time3);
+        System.out.println("Expected Result: 00:00:00"); // Hours, minutes, and seconds all reset to 00
+        System.out.println("Actual Result: " + time3);
+        System.out.println();
+
         time3.tick();
-        System.out.println(time3);
-        time3.tick();
-        System.out.println(time3);
-        Time time4 = new Time(10, 14, 43);
-        System.out.println(time4);
-        Time time5 = new Time(8, 30, 29);
-        time4.add(time5);
-        System.out.println(time4);
-        System.out.println(time5);
-        Time time6 = new Time(7, 20, 0);
-        time4.add(time6);
-        System.out.println(time4);
+        System.out.println("Expected Result: 00:00:01");
+        System.out.println("Actual Result: " + time3);
+        System.out.println();
+
+        Time time4 = new Time(9,50,50); // New time object (09:50:50)
+        System.out.println("Expected Result: 09:50:50");
+        System.out.println("Actual Result: " + time4);
+        System.out.println();
+
+        Time time5 = new Time(10,40,32); // New time object (10:40:32)
+        time4.add(time5); // Adds 09:50:50 to 10:40:32, changing time4
+        System.out.println("Expecred Result: 20:31:22"); // 09:50:50 + 10:40:32 = 20:31:22
+        System.out.println("Actual Result: " + time4);
+        System.out.println();
+
+        Time time6 = new Time(3,29,38); // New time object (03:29:38)
+        time4.add(time6); // adds 03:29:38 to time4 (20:31:32)
+        System.out.println("Expecred Result: 00:01:00"); // 20:31:32 + 03:29:38 = 00:01:00
+        System.out.println("Actual Result: " + time4);
+        System.out.println();
+
+        
+
+
+
+
+    
 
 
 
--- src/Time.java
+++ src/Time.java
@@ -1,16 +1,29 @@
-import java.awt.color.ICC_ColorSpace;
-
+/**
+ * Represents military time (HH:MM:SS)
+ */
 public class Time {
     private int hr;
     private int min;
     private int sec;
 
+/**
+ * Constructor for Time class. Creates instance of Time with given hours, minutes, and seconds
+ * 
+ * @param hr hours (0-23)
+ * @param min minutes (0-59)
+ * @param sec seconds (0-59)
+ */
     public Time(int hr, int min, int sec) {
         this.hr = hr;
         this.min = min;
         this.sec = sec;
     }
 
+    /**
+     * Returns time in HH:MM:SS as a string, pads zero if unit is less than 10
+     * 
+     * @return string representing time
+     */
     public String toString() {
         String time = "";
         if (hr < 10) {
@@ -31,6 +44,9 @@ public class Time {
         return time;
     }
 
+    /**
+     * Increases second by one, changing secs/mins/hrs as necessary
+     */
     public void tick() {
         sec ++;
         if (sec == 60) {
@@ -46,6 +62,11 @@ public class Time {
         }
     }
 
+    /**
+     * Adds time of an instance of Time to this instance's time, changing secs/mins/hrs as necessary
+     * 
+     * @param time Time instance's time that's being added
+     */
     public void add(Time time) {
         sec += time.sec;
         min += time.min;
